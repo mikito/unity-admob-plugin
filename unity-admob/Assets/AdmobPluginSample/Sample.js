101408f528144c73f8750f18a22d2f9be3feec4b
Mikito Yoshiya <mikito0521@gmail.com>
2013-08-16 02:47:33 +0900

    README更新、パッケージング

--- unity-admob/Assets/AdmobPluginSample/Sample.js
+++ unity-admob/Assets/AdmobPluginSample/Sample.js
@@ -1,16 +1,14 @@
 #pragma strict
 
-var admob : AdMobManager;
-
 function OnGUI(){
 	var current : int = 0;
 	if(GUI.Button(Rect(0, (current++) * 100, Screen.width, 100), "Hide Ad")){
-		admob.hide();
+		AdMobManager.instance.hide();
 	}
 	if(GUI.Button(Rect(0, (current++) * 100, Screen.width, 100), "Show Ad")){
-		admob.show();
+		AdMobManager.instance.show();
 	}
 	if(GUI.Button(Rect(0, (current++) * 100, Screen.width, 100), "Refresh Ad")){
-		admob.refresh();
+		AdMobManager.instance.refresh();
 	}
 }
